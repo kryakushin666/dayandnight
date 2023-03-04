1beea7420b678a49ddb7dfa89918c362d60a156c
Artem Kryakushin <artemkryakushin@gmail.com>
2023-03-04 20:35:51 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dayandnight
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dayandnight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dayandnight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
